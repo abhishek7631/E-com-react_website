538197d1251742d0aa8ddedf11e49f2a172fa53e
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-10-22 12:27:43 +0530

    cart code modified

--- app/src/Cart.jsx
+++ app/src/Cart.jsx
@@ -2,30 +2,48 @@ import { useState, useEffect } from "react";
 import axios from "axios"; // Ensure axios is imported
 
 export default function Cart() {
-  const [state, setState] = useState([]);
+  const [data, setData] = useState([]);
 
   const getData = async () => {
-    try {
-      const res = await axios.get("https://fakestoreapi.com/products");
-      setState(res.data);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
+    const res = await axios.get("https://fakestoreapi.com/products");
+    setData(res.data);
   };
 
   useEffect(() => {
-    getData(); // Call getData when the component mounts
+    getData();
   }, []);
 
   return (
-    <div>
-      {state.length > 0 ? (
-        state.map((item) => {
-          return <p key={item.id}>{item.title}</p>;
-        })
-      ) : (
-        <p>Loading...</p>
-      )}
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "space-around",
+      }}
+    >
+      {data.map((item) => (
+        <div
+          key={item.id}
+          style={{
+            marginBottom: "20px",
+            border: "2px solid black",
+            width: "250px",
+            padding: "10px",
+            borderRadius: "20px",
+          }}
+        >
+          <h3>{item.title}</h3>
+          <hr />
+          <img
+            src={item.image}
+            alt={item.title}
+            style={{ width: "150px", height: "200px" }}
+          />
+          <div>
+            <button style={{ marginLeft: "100px" }}>Add to cart</button>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
